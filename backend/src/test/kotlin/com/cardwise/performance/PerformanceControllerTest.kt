7af0e4019d383ead6f2d28a7ea2da39b16cddcd6
crono <cronocros@naver.com>
2026-03-19 11:18:30 +0900

    test(backend): replace deprecated mockbean

--- backend/src/test/kotlin/com/cardwise/performance/PerformanceControllerTest.kt
+++ backend/src/test/kotlin/com/cardwise/performance/PerformanceControllerTest.kt
@@ -19,7 +19,7 @@ import org.mockito.Mockito
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
-import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.test.context.bean.override.mockito.MockitoBean
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.get
 import java.math.BigDecimal
@@ -33,10 +33,10 @@ class PerformanceControllerTest {
     @Autowired
     private lateinit var mockMvc: MockMvc
 
-    @MockBean
+    @MockitoBean
     private lateinit var performanceService: PerformanceService
 
-    @MockBean
+    @MockitoBean
     private lateinit var requestAccountIdResolver: RequestAccountIdResolver
 
     @Test
